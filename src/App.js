5888144710d93289bef6f709f2f0b5d0d7c66640
nevraka <nevrakaya@gmail.com>
2021-12-13 18:47:00 +0300

    App js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import routes from "./config/routes";
+import React, { useContext } from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import SignUp from './components/SignUp';
+import SignIn from './components/SignIn';
+import HomePage from './components/HomePage';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Cart from './components/Cart';
+import Products from './components/Products';
+import ProductDetails from './components/ProductDetails';
+import { UserContext } from './context/app.context';
 
 function App() {
+  const { user } = useContext(UserContext);
+  const navigate = useNavigate();
+
+  const handleAddToCart = () => {
+    if (!user) {
+      navigate('/signup');
+    } else {
+      navigate('/cart');
+    }
+  };
+
   return (
     <div className="App">
+      <Header />
       <Routes>
-        {routes({}).map((route) => (
-          <Route key={route.path} path={route.path} element={route.element} />
-        ))}
+        <Route path="/cart" element={<Cart />}></Route>
+        <Route path="/signin" element={<SignIn />}></Route>
+        <Route path="/signup" element={<SignUp />}></Route>
+        <Route path="/products" element={<Products />}></Route>
+        <Route
+          path="/category/:categoryId"
+          element={<Products handleAddToCart={handleAddToCart} />}
+        ></Route>
+        <Route
+          path="/product/:productId"
+          element={<ProductDetails handleAddToCart={handleAddToCart} />}
+        ></Route>
+        <Route path="/" element={<HomePage />}></Route>
       </Routes>
+      <Footer />
     </div>
   );
 }
