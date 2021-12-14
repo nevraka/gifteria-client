c40525049fc0a820c91e880aa9e0084758f3225e
nevraka <nevrakaya@gmail.com>
2021-12-14 12:35:39 +0300

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,20 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useContext } from 'react';
 import './header.css';
+import { UserContext } from '../context/app.context';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { API_URL } from '../config';
 
 const Header = () => {
+  const { user, setUser } = useContext(UserContext);
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    await axios.post(`${API_URL}/signout`, {}, { withCredentials: true });
+    setUser(null);
+    navigate('/');
+  };
+
   return (
     <div>
       <nav id="nav">
@@ -12,15 +24,32 @@ const Header = () => {
         <div className="b-name">Gifteria</div>
         <div>
           <div>
-            <Link style={{ textDecoration: 'none' }} to="/cart">
-              Cart
+            <Link
+              style={{ textDecoration: 'none', paddingRight: '10px' }}
+              to="/cart"
+            >
+              {user ? `${user.name}'s Cart` : 'Cart'}
             </Link>
           </div>
-          <div>
-            <Link style={{ textDecoration: 'none' }} to="/signin">
-              Login
+          {user ? (
+            <Link style={{ textDecoration: 'none' }} to="/">
+              <div
+                onClick={handleLogout}
+                variant="text"
+                style={{ padding: '0' }}
+              >
+                Logout
+              </div>
             </Link>
-          </div>
+          ) : (
+            <>
+              <div>
+                <Link style={{ textDecoration: 'none' }} to="/signin">
+                  Login
+                </Link>
+              </div>
+            </>
+          )}
         </div>
       </nav>
       <hr />
