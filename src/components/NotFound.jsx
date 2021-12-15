55b1ede68ce2680c6ea525a99f9a43186bb44852
nevraka <nevrakaya@gmail.com>
2021-12-15 21:31:48 +0300

    404 page

---
+++ src/components/NotFound.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div style={{ justifyContent: 'center', display: 'flex' }}>
+      <div>
+        <h1> Page not found!</h1>
+      </div>
+      <img src="./image/404.png" alt="404"></img>
+    </div>
+  );
+};
+
+export default NotFound;
